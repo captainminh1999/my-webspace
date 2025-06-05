4e1c4eac77b7d9ef8543cf20c7a5b3cecb40b206
Minh Nguyen <captainminh1999@gmail.com>
2025-06-05 14:22:21 +1000

    Customize theme colors and base font size

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100`}>
+      <body className={`${inter.className} bg-background text-foreground`}>
         {children}
       </body>
     </html>
